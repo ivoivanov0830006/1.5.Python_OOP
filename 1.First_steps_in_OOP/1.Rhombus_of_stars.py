53b67309e63e4ce5d2095fd0e0f7ed369e7269d8
Ivaylo Ivanov <108527062+ivoivanov0830006@users.noreply.github.com>
2023-02-19 22:36:39 +0200

    Update 1.Rhombus_of_stars.py

--- 1.First_steps_in_OOP/1.Rhombus_of_stars.py
+++ 1.First_steps_in_OOP/1.Rhombus_of_stars.py
@@ -24,3 +24,33 @@ class Rhombus:
 n = int(input())
 rhombus = Rhombus(n).main()
 print(rhombus)
+
+
+"""
+------------------------------------- Another Solution -----------------------------
+
+def print_row(n, row):
+    for space in range(n - row):
+        print(" ", end="")
+    for star in range(1, row):
+        print("*", end=" ")
+    print("*")
+
+
+def print_upper(n):
+    for row in range(1, n + 1):
+        print_row(n, row)
+
+
+def print_lower(n):
+    for row in range(n - 1, 0, -1):
+        print_row(n, row)
+
+
+def print_rhombus(n):
+    print_upper(n)
+    print_lower(n)
+
+
+n = int(input())
+print_rhombus(n)
