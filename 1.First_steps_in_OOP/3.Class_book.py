6ff28cb7388df0955c48b2369c1abab8e8d06fc3
Ivaylo Ivanov <108527062+ivoivanov0830006@users.noreply.github.com>
2023-02-19 23:02:35 +0200

    Update 3.Class_book.py

--- 1.First_steps_in_OOP/3.Class_book.py
+++ 1.First_steps_in_OOP/3.Class_book.py
@@ -9,3 +9,24 @@ book = Book("My Book", "Me", 200)
 print(book.name)
 print(book.author)
 print(book.pages)
+
+
+"""
+------------------------------------ Problem to resolve --------------------------------
+
+Create a class called Book. It should have an __init__() method that should receive:
+        name: string
+        author: string
+        pages: int
+-------------------------------------- Example inputs ----------------------------------
+Test Code	
+book = Book("My Book", "Me", 200)
+print(book.name)
+print(book.author)
+print(book.pages)	
+Output
+My Book
+Me
+200
+
+"""
