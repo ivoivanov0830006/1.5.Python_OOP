019fdad061d34836c8172bf7b0c0044663a3fd56
Ivaylo Ivanov <108527062+ivoivanov0830006@users.noreply.github.com>
2023-02-19 23:04:45 +0200

    Update 2.Scope_mess.py

--- 1.First_steps_in_OOP/2.Scope_mess.py
+++ 1.First_steps_in_OOP/2.Scope_mess.py
@@ -5,34 +5,6 @@ def outer():
     x = "local"
 
     def inner():
-        x = "nonlocal"
-        print("inner:", x)
-
-    def change_global():
-        x = "global: changed!"
-
-    print("outer:", x)
-    inner()
-    print("outer:", x)
-    change_global()
-
-
-print(x)
-outer()
-print(x)
-
-
-"""
----------------------------------------- Solution --------------------------------------
-"""
-
-x = "global"
-
-
-def outer():
-    x = "local"
-
-    def inner():
         nonlocal x          # added
         x = "nonlocal"
         print("inner:", x)
@@ -50,3 +22,46 @@ def outer():
 print(x)
 outer()
 print(x)
+
+"""
+------------------------------------ Problem to resolve --------------------------------
+
+Fix the code below, so it returns the expected output:
+
+x = "global"
+
+
+def outer():
+    x = "local"
+
+    def inner():
+        x = "nonlocal"
+        print("inner:", x)
+
+    def change_global():
+        x = "global: changed!"
+
+    print("outer:", x)
+    inner()
+    print("outer:", x)
+    change_global()
+
+
+print(x)
+outer()
+print(x)
+-------------------------------------- Example inputs ----------------------------------
+Current Output	
+global
+outer: local
+inner: nonlocal
+outer: local
+global	
+Expected Output
+global
+outer: local
+inner: nonlocal
+outer: nonlocal
+global: changed!
+
+"""
