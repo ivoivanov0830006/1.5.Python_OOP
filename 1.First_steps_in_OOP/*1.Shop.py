1c76405a8f9294f376351906ad70828cc335fbee
Ivaylo Ivanov <108527062+ivoivanov0830006@users.noreply.github.com>
2023-02-22 22:14:19 +0200

    Update *1.Shop.py

--- 1.First_steps_in_OOP/*1.Shop.py
+++ 1.First_steps_in_OOP/*1.Shop.py
@@ -10,3 +10,18 @@ class Shop:
 
 shop = Shop("My Shop", ["Apples", "Bananas", "Cucumbers"])
 print(shop.get_items_count())
+
+
+"""
+------------------------------------ Problem to resolve --------------------------------
+
+Create a class called Shop. Upon initialization, it should receive a name (string) and items 
+(list). Create a method called get_items_count() which should return the number of items in the store.
+-------------------------------------- Example inputs ----------------------------------
+Test Code	
+shop = Shop("My Shop", ["Apples", "Bananas", "Cucumbers"])
+print(shop.get_items_count())	
+Output
+3
+
+"""
