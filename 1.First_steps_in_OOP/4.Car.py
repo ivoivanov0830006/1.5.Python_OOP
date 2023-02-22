62a48f65f78fd497c48f0d7d6f43b301e063b5e7
Ivaylo Ivanov <108527062+ivoivanov0830006@users.noreply.github.com>
2023-02-22 22:09:03 +0200

    Update 4.Car.py

--- 1.First_steps_in_OOP/4.Car.py
+++ 1.First_steps_in_OOP/4.Car.py
@@ -10,3 +10,20 @@ class Car():
 
 car = Car("Kia", "Rio", "1.3L B3 I4")
 print(car.get_info())
+
+
+"""
+------------------------------------ Problem to resolve --------------------------------
+
+Create a class called Car. Upon initialization, it should receive a name, model, and engine 
+(all strings). Create a method called get_info() which will return a string in the following 
+format:
+        "This is {name} {model} with engine {engine}".
+-------------------------------------- Example inputs ----------------------------------
+----Test Code-----
+car = Car("Kia", "Rio", "1.3L B3 I4")
+print(car.get_info())	
+----Output-----
+This is Kia Rio with engine 1.3L B3 I4
+
+"""
