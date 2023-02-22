85ab7c7f69eea5c09d10d416e9de9ee2cc6c3364
Ivaylo Ivanov <108527062+ivoivanov0830006@users.noreply.github.com>
2023-02-22 22:20:07 +0200

    Update *3.Employee.py

--- 1.First_steps_in_OOP/*3.Employee.py
+++ 1.First_steps_in_OOP/*3.Employee.py
@@ -21,3 +21,25 @@ employee = Employee(744423129, "John", "Smith", 1000)
 print(employee.get_full_name())
 print(employee.raise_salary(500))
 print(employee.get_annual_salary())
+
+
+"""
+------------------------------------ Problem to resolve --------------------------------
+
+Create class Employee. Upon initialization, it should receive id (number), first_name (string), 
+last_name (string) and salary (number). Create 3 additional instance methods:
+⦁	get_full_name() - returns "{first_name} {last_name}"
+⦁	get_annual_salary() - returns the total salary for 12 months
+⦁	raise_salary(amount) - increases the salary by the given amount and returns the new salary
+-------------------------------------- Example inputs ----------------------------------
+Test Code	
+employee = Employee(744423129, "John", "Smith", 1000)
+print(employee.get_full_name())
+print(employee.raise_salary(500))
+print(employee.get_annual_salary())	
+Output
+John Smith
+1500
+18000
+
+"""
