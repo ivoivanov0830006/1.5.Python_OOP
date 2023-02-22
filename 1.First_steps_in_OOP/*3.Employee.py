541cd8f8469dc7ffac1407c5790443dfe69f68fc
Ivaylo Ivanov <108527062+ivoivanov0830006@users.noreply.github.com>
2023-02-22 22:19:55 +0200

    Create *3.Employee.py

---
+++ 1.First_steps_in_OOP/*3.Employee.py
@@ -0,0 +1,23 @@
+class Employee:
+    def __init__(self, id, first_name, last_name, salary):
+        self.id = id
+        self.first_name = first_name
+        self.last_name = last_name
+        self.salary = salary
+
+    def get_full_name(self):
+        return f"{self.first_name} {self.last_name}"
+
+    def raise_salary(self, current_raise):
+        self.salary += current_raise
+        return self.salary
+
+    def get_annual_salary(self):
+        result = 12 * self.salary
+        return result
+
+
+employee = Employee(744423129, "John", "Smith", 1000)
+print(employee.get_full_name())
+print(employee.raise_salary(500))
+print(employee.get_annual_salary())
