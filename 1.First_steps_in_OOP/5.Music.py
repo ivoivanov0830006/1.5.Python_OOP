3278c69a0af8ae4d8c6d625b54874ed8852ffebd
Ivaylo Ivanov <108527062+ivoivanov0830006@users.noreply.github.com>
2023-02-22 22:11:24 +0200

    Update 5.Music.py

--- 1.First_steps_in_OOP/5.Music.py
+++ 1.First_steps_in_OOP/5.Music.py
@@ -14,3 +14,24 @@ class Music():
 song = Music("Title", "Artist", "Lyrics")
 print(song.print_info())
 print(song.play())
+
+
+"""
+------------------------------------ Problem to resolve --------------------------------
+
+Create a class named Music that receives title (string), artist (string), and lyrics (string) 
+upon initialization. The class should also have two additional methods:
+    * The print_info() method should return the following: 
+        'This is "{title}" from "{artist}"'
+    * The play() method should return the lyrics.
+Submit only the class in the judge system. Test your code with your own examples.
+-------------------------------------- Example inputs ----------------------------------
+Test Code	
+song = Music("Title", "Artist", "Lyrics")
+print(song.print_info())
+print(song.play())	
+Output
+This is "Title" from "Artist"
+Lyrics
+
+"""
