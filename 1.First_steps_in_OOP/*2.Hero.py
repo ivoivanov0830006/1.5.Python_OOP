eb6a441a3585bd882c3b41c5b6ab13380bf309ea
Ivaylo Ivanov <108527062+ivoivanov0830006@users.noreply.github.com>
2023-02-22 22:17:38 +0200

    Update *2.Hero.py

--- 1.First_steps_in_OOP/*2.Hero.py
+++ 1.First_steps_in_OOP/*2.Hero.py
@@ -18,3 +18,24 @@ print(hero.defend(50))
 hero.heal(50)
 print(hero.defend(99))
 print(hero.defend(1))
+
+
+"""
+------------------------------------ Problem to resolve --------------------------------
+
+Create a class called Hero. Upon initialization, it should receive a name (string) and health 
+(number). Create two additional methods:
+⦁	defend(damage) - reduce the given damage from the hero's health:
+⦁	if the health become 0 or less, set it to 0 and return "{name} was defeated"
+⦁	heal(amount) - increase the health of the hero with the given amount
+-------------------------------------- Example inputs ----------------------------------
+Test Code	Output
+hero = Hero("Peter", 100)
+print(hero.defend(50))
+hero.heal(50)
+print(hero.defend(99))
+print(hero.defend(1))	None
+None
+Peter was defeated
+
+"""
