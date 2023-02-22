fa08351f6a5006b0554b800f3c708fe5db640314
Ivaylo Ivanov <108527062+ivoivanov0830006@users.noreply.github.com>
2023-02-22 22:18:06 +0200

    Update *2.Hero.py

--- 1.First_steps_in_OOP/*2.Hero.py
+++ 1.First_steps_in_OOP/*2.Hero.py
@@ -29,12 +29,14 @@ Create a class called Hero. Upon initialization, it should receive a name (strin
 ⦁	if the health become 0 or less, set it to 0 and return "{name} was defeated"
 ⦁	heal(amount) - increase the health of the hero with the given amount
 -------------------------------------- Example inputs ----------------------------------
-Test Code	Output
+Test Code	
 hero = Hero("Peter", 100)
 print(hero.defend(50))
 hero.heal(50)
 print(hero.defend(99))
-print(hero.defend(1))	None
+print(hero.defend(1))	
+Output
+None
 None
 Peter was defeated
 
