642b1a690f545a7d7a4f589757d9439ad041b83a
Ivaylo Ivanov <108527062+ivoivanov0830006@users.noreply.github.com>
2023-03-05 16:52:41 +0200

    Update *5.Flower.py

--- 1.First_steps_in_OOP/*5.Flower.py
+++ 1.First_steps_in_OOP/*5.Flower.py
@@ -25,3 +25,31 @@ flower.water(60)
 print(flower.status())
 flower.water(100)
 print(flower.status())
+
+
+"""
+------------------------------------ Problem to resolve --------------------------------
+
+Create a class called Flower. Upon initialization, the class should receive a name (string) and a 
+water_requirements (number). The flower should also have an instance attribute called is_happy (False 
+by default). Add two additional methods to the class:
+⦁	water(quantity) - it will water the flower. Each time check if the quantity is greater than or equal 
+to the required. If it is - the flower becomes happy (set is_happy to True).
+⦁	status() - it should return "{name} is happy" if the flower is happy, otherwise it should return 
+"{name} is not happy".
+
+-------------------------------------- Example inputs ----------------------------------
+Test Code	
+flower = Flower("Lilly", 100)
+flower.water(50)
+print(flower.status())
+flower.water(60)
+print(flower.status())
+flower.water(100)
+print(flower.status())	
+Output
+Lilly is not happy
+Lilly is not happy
+Lilly is happy
+
+"""
