7ced70d3f5dae8be5e6fd6897a040277e1d8a56a
Ivaylo Ivanov <108527062+ivoivanov0830006@users.noreply.github.com>
2023-03-05 16:50:40 +0200

    Update *6.Steam_user.py

--- 1.First_steps_in_OOP/*6.Steam_user.py
+++ 1.First_steps_in_OOP/*6.Steam_user.py
@@ -1,5 +1,38 @@
+class SteamUser:
+    def __init__(self, username, games):
+        self.username = username
+        self.games = games
+        self.played_hours = 0
+
+    def play(self, current_game, current_hours):
+        if current_game in self.games:
+            self.played_hours += current_hours
+            return f"{self.username} is playing {current_game}"
+        return f"{current_game} is not in library"
+
+    def buy_game(self, game):
+        if game not in self.games:
+            self.games.append(game)
+            return f"{self.username} bought {game}"
+        return f"{game} is already in your library"
+
+    def status(self):
+        games_count = len(self.games)
+        return f"{self.username} has {games_count} games. Total play time: {self.played_hours}"
+
+
+user = SteamUser("Peter", ["Rainbow Six Siege", "CS:GO", "Fortnite"])
+print(user.play("Fortnite", 3))
+print(user.play("Oxygen Not Included", 5))
+print(user.buy_game("CS:GO"))
+print(user.buy_game("Oxygen Not Included"))
+print(user.play("Oxygen Not Included", 6))
+print(user.status())
+
+
 """
 ------------------------------------ Problem to resolve --------------------------------
+
 Create a class called SteamUser. Upon initialization, it should receive a username (string) and games 
 (list). It should also have an attribute called played_hours (0 by default). Add three methods to the class:
 ⦁	play(game, hours)
@@ -12,6 +45,7 @@ is playing {game}"
 ⦁	status() - returns the following:
     "{username} has {games_count} games. Total play time: {played_hours}"
 Submit only the class in the judge system.
+
 -------------------------------------- Example inputs ----------------------------------
 Test Code	
 user = SteamUser("Peter", ["Rainbow Six Siege", "CS:GO", "Fortnite"])
