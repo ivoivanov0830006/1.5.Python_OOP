860c77b73df71addc073a81a79d3570eaaab1254
Ivaylo Ivanov <108527062+ivoivanov0830006@users.noreply.github.com>
2023-03-13 15:31:28 +0200

    Update 6.Formula_1_manager.py

--- 6.Polymorphism_and_Abstraction/6.Formula_1_manager.py
+++ 6.Polymorphism_and_Abstraction/6.Formula_1_manager.py
@@ -88,3 +88,25 @@ Note: There won't be a case where a valid team tries to register for a second ti
                 "Red Bull: { Red Bull revenue message }. Mercedes: { Mercedes revenue message }.
                 { team with better position } is ahead at the { race name } race."
 Note: Teams' positions will always be valid.
+
+-------------------------------------- Example inputs ----------------------------------
+Test Code
+from project.f1_season_app import F1SeasonApp
+
+f1_season = F1SeasonApp()
+
+print(f1_season.register_team_for_season("Red Bull", 2000000))
+print(f1_season.register_team_for_season("Mercedes", 2500000))
+print(f1_season.new_race_results("Nurburgring", 1, 7))
+print(f1_season.new_race_results("Silverstone", 10, 1))
+Output
+Red Bull has joined the new F1 season.
+Mercedes has joined the new F1 season.
+Red Bull: The revenue after the race is 1270000$. Current budget 3270000$.
+Mercedes: The revenue after the race is -150000$. Current budget 2350000$.
+Red Bull is ahead at the Nurburgring race.
+Red Bull: The revenue after the race is -240000$. Current budget 3030000$.
+Mercedes: The revenue after the race is 900000$. Current budget 3250000$.
+Mercedes is ahead at the Silverstone race.
+
+"""
