cf8d871fb9f24080c1e5ba0af32c847aaaa72934
Ivaylo Ivanov <108527062+ivoivanov0830006@users.noreply.github.com>
2023-03-14 14:44:26 +0200

    Update 3.Prisoner.py

--- 7.SOLID/3.Prisoner.py
+++ 7.SOLID/3.Prisoner.py
@@ -36,3 +36,56 @@ except:
 
 print(f"The location of the prison: {prisoner.PRISON_LOCATION}")
 print(f"The current position of the prisoner: {prisoner.position}")
+
+
+"""
+------------------------------------ Problem to resolve --------------------------------
+
+You are provided with a code containing a class Prisoner and a class Person. A prisoner is obviously a 
+person, but since a prisoner is not free to move an arbitrary distance, the Person class can be named 
+FreePerson, then the idea that a Prisoner inherits FreePerson is wrong. Rewrite the code and apply the LSP 
+(Liskov Substitution Principle).
+
+-------------------------------------- Example inputs ----------------------------------
+Code Before: 
+
+class Worker:
+
+    def work(self):
+        print("I'm working!!")
+
+
+class Manager:
+
+    def __init__(self):
+        self.worker = None
+
+    def set_worker(self, worker):
+        assert isinstance(worker, Worker), '`worker` must be of type {}'.format(Worker)
+
+        self.worker = worker
+
+    def manage(self):
+        if self.worker is not None:
+            self.worker.work()
+
+class SuperWorker:
+
+    def work(self):
+        print("I work very hard!!!")
+
+
+
+worker = Worker()
+manager = Manager()
+manager.set_worker(worker)
+manager.manage()
+
+super_worker = SuperWorker()
+try:
+    manager.set_worker(super_worker)
+except AssertionError:
+    print("manager fails to support super_worker....")
+
+
+-------------------------------------------------------------------------------------
