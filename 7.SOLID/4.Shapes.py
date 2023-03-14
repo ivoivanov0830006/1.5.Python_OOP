5cccc74b044abd28503e1f715afbf4aa9663435b
Ivaylo Ivanov <108527062+ivoivanov0830006@users.noreply.github.com>
2023-03-14 14:45:36 +0200

    Update 4.Shapes.py

--- 7.SOLID/4.Shapes.py
+++ 7.SOLID/4.Shapes.py
@@ -58,3 +58,49 @@ print("The total area is: ", calculator.total_area)
 You are provided with code containing class Rectangle and class AreaCalculator. Refactor the 
 code using the Open/Closed Principle so that the code is open for extension (adding more shapes) 
 but closed for modification.
+
+-------------------------------------- Example inputs ----------------------------------
+Code Before: 
+
+class Rectangle:
+
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+
+class AreaCalculator:
+
+    def __init__(self, shapes):
+
+        assert isinstance(shapes, list), "`shapes` should be of type `list`."
+        self.shapes = shapes
+
+    @property
+    def total_area(self):
+        total = 0
+        for shape in self.shapes:
+            total += shape.width * shape.height
+
+        return total
+
+
+shapes = [Rectangle(2, 3), Rectangle(1, 6)]
+calculator = AreaCalculator(shapes)
+print("The total area is: ", calculator.total_area)
+
+-------------------------------------------------------------------------------------
+Test Code Before:                                                   Output Before:
+shapes = [Rectangle(2, 3), Rectangle(1, 6)]                         The total area is:  12
+calculator = AreaCalculator(shapes)
+print("The total area is: ", calculator.total_area)
+
+
+-------------------------------------------------------------------------------------
+Test Code After:                                                    Output Before:
+shapes = [Rectangle(1, 6), Triangle(2, 3)]                          The total area is:  9.0
+calculator = AreaCalculator(shapes)
+
+print("The total area is: ", calculator.total_area)
+
+
+"""
