71dd0b8f4bc0859d1523663a7dfb8f8d965deadc
Ivaylo Ivanov <108527062+ivoivanov0830006@users.noreply.github.com>
2023-03-15 18:43:47 +0200

    Update 4.Test_car.py

--- 10.Testing/4.Test_car.py
+++ 10.Testing/4.Test_car.py
@@ -66,24 +66,21 @@ if __name__ == "__main__":
 """
 ------------------------------------ Problem to resolve --------------------------------
 
-You are provided with a class IntegerList. It should only store integers. The initial integers should
-be set by the constructor. They are stored as a list. IntegerList has a functionality to add, remove_index,
-get, insert, get the biggest number, and get the index of an element. Your task is to test the class.
-Note: You are not allowed to change the structure of the provided code.
+You are provided with a simple project containing only one class - Car. The provided class is 
+simple - its main point is to represent some of the functionality of a Car. Each car contains 
+information about its make, model, fuel consumption, fuel amount, and fuel capacity. Also, each 
+Car can add some fuel to its tank by refueling and can travel distance by driving. In order to be 
+driven, our Car needs to have enough fuel. Everything in the provided skeleton is working perfectly 
+fine, and you mustn't change it.
+Your job now is to write unit tests on the provided project and its functionality. You should test 
+every part of the code inside the Car class:
+· You should test the constructor
+· You should test all the methods and validations inside the class
 Constraints
-•	add operation, should add an element and returns the list.
-    *	If the element is not an integer, a ValueError is thrown
-•	remove_index operation removes the element on that index and returns it.
-    *	If the index is out of range, an IndexError is thrown
-•	__init__ should only take integers, and store them
-•	get should return the specific element
-    *	If the index is out of range, an IndexError is thrown
-•	insert
-    *	If the index is out of range, IndexError is thrown
-    *	If the element is not an integer, ValueError is thrown
-•	get_biggest
-•	get_index
-Hint: Do not forget to test the constructor
+· Everything in the provided skeleton is working perfectly fine
+· You must not change anything in the project structure
+· Any part of validation should be tested
+· There is no limit on the tests you can write but keep your attention on the main functionality
 
 -------------------------------------- Example inputs ----------------------------------
 
