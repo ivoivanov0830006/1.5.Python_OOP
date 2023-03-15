30c1b066903c9d5f5a1015e2deb577dbd351031e
Ivaylo Ivanov <108527062+ivoivanov0830006@users.noreply.github.com>
2023-03-15 12:45:07 +0200

    Update 1.Test_worker.py

--- 10.Testing/1.Test_worker.py
+++ 10.Testing/1.Test_worker.py
@@ -36,3 +36,43 @@ class WorkerTests(TestCase):
 
 if __name__ == "__main__":
     main()
+
+    
+"""
+------------------------------------ Problem to resolve --------------------------------
+
+Create a class WorkerTests
+In judge you need to submit just the WokerTests class, with the unittest module imported.
+Create the following tests:
+•	# 1 # Test if the worker is initialized with the correct name, salary, and energy
+•	# 2 # Test if the worker's energy is incremented after the rest method is called
+•	# 3 # Test if an error is raised if the worker tries to work with negative energy or equal to 0
+•	# 4 # Test if the worker's money is increased by his salary correctly after the work method is called
+•	# 5 # Test if the worker's energy is decreased after the work method is called	
+•	# 6 # Test if the get_info method returns the proper string with correct values
+
+
+-------------------------------------- Example inputs ----------------------------------
+
+class Worker:
+
+    def __init__(self, name, salary, energy):
+        self.name = name
+        self.salary = salary
+        self.energy = energy
+        self.money = 0
+
+    def work(self):
+        if self.energy <= 0:
+            raise Exception('Not enough energy.')
+
+        self.money += self.salary
+        self.energy -= 1
+
+    def rest(self):
+        self.energy += 1
+
+    def get_info(self):
+        return f'{self.name} has saved {self.money} money.'
+        
+"""
