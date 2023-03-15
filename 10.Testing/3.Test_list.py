b0a31dd1cc3c4e5d00aaa11d13bc76ca1f9b01cb
Ivaylo Ivanov <108527062+ivoivanov0830006@users.noreply.github.com>
2023-03-15 13:35:50 +0200

    Update 3.Test_list.py

--- 10.Testing/3.Test_list.py
+++ 10.Testing/3.Test_list.py
@@ -63,3 +63,27 @@ class IntegerListTest(TestCase):
 
 if __name__ == "__main__":
     main()
+
+    
+"""
+------------------------------------ Problem to resolve --------------------------------
+
+You are provided with a class IntegerList. It should only store integers. The initial integers should
+be set by the constructor. They are stored as a list. IntegerList has a functionality to add, remove_index,
+get, insert, get the biggest number, and get the index of an element. Your task is to test the class.
+Note: You are not allowed to change the structure of the provided code.
+
+Constraints
+•	add operation, should add an element and returns the list.
+    *	If the element is not an integer, a ValueError is thrown
+•	remove_index operation removes the element on that index and returns it.
+    *	If the index is out of range, an IndexError is thrown
+•	__init__ should only take integers, and store them
+•	get should return the specific element
+    *	If the index is out of range, an IndexError is thrown
+•	insert
+    *	If the index is out of range, IndexError is thrown
+    *	If the element is not an integer, ValueError is thrown
+•	get_biggest
+•	get_index
+Hint: Do not forget to test the constructor
