41f4f7e72236c404cfaaf92ef3a3cf5381a804d0
Ivaylo Ivanov <108527062+ivoivanov0830006@users.noreply.github.com>
2023-03-24 17:17:32 +0200

    Update *2.Workers_2.py

--- 7.SOLID/*2.Workers_2/*2.Workers_2.py
+++ 7.SOLID/*2.Workers_2/*2.Workers_2.py
@@ -66,7 +66,6 @@ class BreakManager(BaseManager):
 
 work_manager = WorkManager()
 break_manager = BreakManager()
-
 work_manager.set_worker(Worker())
 break_manager.set_worker(Worker())
 work_manager.manage()
@@ -78,9 +77,12 @@ work_manager.manage()
 break_manager.lunch_break()
 
 work_manager.set_worker(Robot())
-break_manager.set_worker(Robot())
 work_manager.manage()
-break_manager.lunch_break()
+try:
+    break_manager.set_worker(Robot())
+    break_manager.lunch_break()
+except:
+    pass
 
 
 """
