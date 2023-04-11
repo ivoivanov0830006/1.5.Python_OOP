ad7cf0ea7087c4e1f5ad2231b0f4861cd79a8a2c
Ivaylo Ivanov <108527062+ivoivanov0830006@users.noreply.github.com>
2023-04-12 00:36:35 +0300

    Add files via upload

---
+++ 0.Exams/1.Bakery_tables/project/baked_food/bread.py
@@ -0,0 +1,8 @@
+from project import BakedFood
+
+
+class Bread(BakedFood):
+    DEFAULT_PORTION = 200
+
+    def __init__(self, name: str, price: float):
+        super().__init__(name, self.DEFAULT_PORTION, price)
