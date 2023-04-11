4512a38d55b7fe281b828248513a93dfabc5211a
Ivaylo Ivanov <108527062+ivoivanov0830006@users.noreply.github.com>
2023-04-12 00:41:26 +0300

    Add files via upload

---
+++ 0.Exams/1.Bakery_tables/project/drink/water.py
@@ -0,0 +1,8 @@
+from project import Drink
+
+
+class Water(Drink):
+    DEFAULT_PRICE = 1.5
+
+    def __init__(self, name: str, portion: float, brand: str):
+        super().__init__(name, portion, self.DEFAULT_PRICE, brand)
