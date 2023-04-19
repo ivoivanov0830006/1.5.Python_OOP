1cd6199a640cc7ca89e032b4d688f3a68ede6d8e
Ivaylo Ivanov <108527062+ivoivanov0830006@users.noreply.github.com>
2023-04-19 20:55:04 +0200

    Update concert.py

--- 0.Exams/1.Concert_tracker/project/concert.py
+++ 0.Exams/1.Concert_tracker/project/concert.py
@@ -1,3 +1,9 @@
+class Concert:
+    def __init__(self, genre: str, audience: int, ticket_price: float, expenses: float, place: str):
+        self.genre = genre
+        self.audience = audience
+        self.ticket_price = ticket_price
+        self.expenses = expenses
         self.place = place
 
     @property
