659ee4eb9f6158b8579e4fbd48152bd7c14142f3
Ivaylo Ivanov <108527062+ivoivanov0830006@users.noreply.github.com>
2023-04-25 23:57:22 +0300

    Create client.py

---
+++ 0.Exams/1.Food_orders/project/client.py
@@ -0,0 +1,20 @@
+class Client:
+    def __init__(self, phone_number: str):
+        self.phone_number = phone_number
+        self.shopping_cart = []
+        self.bill = 0
+        self.ordered_meals = {}
+
+    @property
+    def phone_number(self):
+        return self.__phone_number
+
+    @phone_number.setter
+    def phone_number(self, value):
+        if not value.startswith("0"):
+            raise ValueError("Invalid phone number!")
+        elif not len(value) == 10:
+            raise ValueError("Invalid phone number!")
+        elif not value.isnumeric():
+            raise ValueError("Invalid phone number!")
+        self.__phone_number = value
