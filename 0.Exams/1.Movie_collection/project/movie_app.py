22d2a23c7a8303b237b63789d2ab171109628102
Ivaylo Ivanov <108527062+ivoivanov0830006@users.noreply.github.com>
2023-05-03 22:23:08 +0300

    Update movie_app.py

--- 0.Exams/1.Movie_collection/project/movie_app.py
+++ 0.Exams/1.Movie_collection/project/movie_app.py
@@ -32,3 +32,25 @@ class MovieApp:
         current_user = User(username, age)
         self.users_collection.append(current_user)
         return f"{username} registered successfully."
+
+    def upload_movie(self, username: str, movie):
+        if not self.__user_is_added(username):
+            raise Exception("This user does not exist!")
+        elif not movie.owner.username == username:
+            raise Exception(f"{username} is not the owner of the movie {movie.title}!")
+        elif self.__movie_is_added(movie.title):
+            raise Exception("Movie already added to the collection!")
+        self.movies_collection.append(movie)
+        for user in self.users_collection:
+            if user.username == username:
+                user.movies_owned.append(movie)
+                return f"{username} successfully added {movie.title} movie."
+
+    def edit_movie(self, username: str, movie, **kwargs):
+        if not self.__movie_is_added(movie.title):
+            raise Exception(f"The movie {movie.title} is not uploaded!")
+        elif not movie.owner.username == username:
+            raise Exception(f"{username} is not the owner of the movie {movie.title}!")
+        for key, value in kwargs.items():  # key = attribute, value = new value
+            setattr(movie, key, value)     # movie = object, key = attribute, value = new value
+        return f"{username} successfully edited {movie.title} movie."
